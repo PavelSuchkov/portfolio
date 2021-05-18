9b5d58e3fb26ce7a55b95c0af2166fe24d9ae959
PavelSuchkov <suchkovp@gmail.com>
2021-05-18 15:42:56 +0300

    some cosmetic refactorings

--- src/myProjects/projectItem/ProjectItem.tsx
+++ src/myProjects/projectItem/ProjectItem.tsx
@@ -10,12 +10,12 @@ export type ProjectItemPropsType = {
 
 export function ProjectItem(props: ProjectItemPropsType) {
 
+
+
     return (
         <div className={style.project}>
             <div className={style.image} style={props.style}>
                 <a href="" className={style.viewBtn}>See</a>
-                {/*<div className={style.topWrapper}>*/}
-                {/*</div>*/}
             </div>
             <div className={style.projectInfo}>
                 <div className={style.projectTitle}>{props.title}</div>
