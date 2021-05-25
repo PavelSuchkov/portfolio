ba91353ba6c45273997b9596cc681eb1113d78d6
PavelSuchkov <suchkovp@gmail.com>
2021-05-25 14:05:37 +0300

    scss was added

--- src/freelance/Freelance.tsx
+++ src/freelance/Freelance.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./Freelance.module.css";
+import style from "./Freelance.module.scss";
 import {Title} from "../common/components/title/Title";
 
 export function Freelance() {
